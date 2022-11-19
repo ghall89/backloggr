0bcf9950ff14cbe8be1e84a69aee739b2a590c08
Graham Hall <ghall89@me.com>
2022-11-19 17:03:22 -0500

    fixed config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
 	reactStrictMode: true,
 	env: {
 		MONGO_DB: process.env.MONGO_DB,
+		RAWG_API_KEY: process.env.RAWG_API_KEY,
 	},
 };
 
--- src/lib/rawgApi.js
+++ src/lib/rawgApi.js
@@ -1,6 +1,6 @@
 const gameSearch = async query => {
 	const res = await fetch(
-		`http://api.rawg.io/api/games?key=3859c8c833894695b672a8a11303911a&search=${query}`,
+		`http://api.rawg.io/api/games?key=${process.env.RAWG_API_KEY}&search=${query}`,
 	)
 		.then(response => response.json())
 		.catch(err => console.error(err));
