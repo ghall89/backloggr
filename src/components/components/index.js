25df4115c0b67e8f15b1efae2ba971d45e9266d7
Graham Hall <graham@ghall.dev>
2022-11-27 15:42:36 -0500

    design tweaks and placeholder img

--- src/components/components/index.js
+++ src/components/components/index.js
@@ -3,5 +3,13 @@ import AppBar from './AppBar';
 import BottomTabs from './BottomTabs';
 import ConfirmModal from './ConfirmModal';
 import GameList from './GameList';
+import PlatformIcon from './PlatformIcon';
 
-export { AppBar, AddGameModal, BottomTabs, ConfirmModal, GameList };
+export {
+	AppBar,
+	AddGameModal,
+	BottomTabs,
+	ConfirmModal,
+	GameList,
+	PlatformIcon,
+};
