2277f037974f0c067cdff94f4a6b17da0cc04d6f
Graham Hall <graham@ghall.dev>
2022-12-11 15:12:27 -0500

    integrated rawg api

--- src/components/components/AddGameModal.js
+++ src/components/components/AddGameModal.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import { useUser } from '@auth0/nextjs-auth0';
 import {
 	Box,
@@ -10,36 +10,98 @@ import {
 	MenuItem,
 	Select,
 	TextField,
+	Typography,
+	Link,
 } from '@mui/material';
 
-import { currentPlatformList, oldPlatformList } from '../../lib/platformList';
+const gameSearch = async query => {
+	const res = await fetch(
+		`https://api.rawg.io/api/games?key=${process.env.RAWG_API_KEY}&search=${query}`,
+	)
+		.then(response => response.json())
+		.catch(err => console.error(err));
+
+	return res;
+};
 
 const AddGameModal = ({ openModal, setOpenModal, addAction }) => {
 	const { user } = useUser();
 
-	const [title, setTitle] = useState(null);
-	const [platform, setPlatform] = useState(null);
+	const [query, setQuery] = useState();
+	const [searchResults, setSearchResults] = useState([]);
+	const [selectedGame, setSelectedGame] = useState();
+	const [gamePlatforms, setGamePlatforms] = useState([]);
+	const [selectedPlatform, setSelectedPlatform] = useState();
 
-	const handleSubmit = async () => {
-		if (title && platform) {
-			const submitBody = {
-				title,
-				platform,
-				status: 'not_started',
-				user_ref: user.sub,
-				starred: false,
-			};
-			await addAction(submitBody);
-			setTitle(null);
-			setPlatform(null);
-			setOpenModal(false);
-		}
+	const gamePlatformsMemo = useMemo(() => {
+		searchResults.forEach(game => {
+			if (selectedGame === game.id) {
+				return game.platforms;
+			}
+		});
+	}, [selectedGame, searchResults]);
+
+	const handleSearch = async () => {
+		const res = await gameSearch(query);
+		await setSearchResults(res.results);
+		setSelectedGame(res.results[0].id);
 	};
 
 	const handleChange = async event => await setPlatform(event.target.value);
 
+	useEffect(() => {
+		searchResults.forEach(game => {
+			if (selectedGame === game.id) {
+				setGamePlatforms(game.platforms);
+				setSelectedPlatform(game.platforms[0].platform.name);
+			}
+		});
+	}, [selectedGame]);
+
+	const handleClearState = () => {
+		setQuery();
+		setSearchResults([]);
+		setSelectedGame();
+		setGamePlatforms([]);
+		setSelectedPlatform();
+	};
+
+	const handleClose = () => {
+		handleClearState();
+		setOpenModal(false);
+	};
+
+	const handleSubmit = async () => {
+		let gameData = {
+			status: 'not_started',
+			user_ref: user.sub,
+			starred: false,
+		};
+
+		searchResults.forEach(game => {
+			if (selectedGame === game.id) {
+				gameData = {
+					...gameData,
+					rawg_id: game.id,
+					title: game.name,
+					platform: selectedPlatform,
+					img: game.background_image,
+					avg_playtime: game.playtime,
+					metacritic: game.metacritic,
+					genres: game.genres,
+				};
+				return;
+			}
+		});
+
+		console.log(gameData);
+
+		await addAction(gameData);
+		handleClose();
+	};
+
 	return (
-		<Dialog onClose={() => setOpenModal(false)} open={openModal}>
+		<Dialog onClose={handleClose} open={openModal}>
 			<Box
 				sx={{
 					padding: 3,
@@ -51,37 +113,61 @@ const AddGameModal = ({ openModal, setOpenModal, addAction }) => {
 					width: '300px',
 				}}
 			>
-				<TextField
-					onChange={({ target }) => setTitle(target.value)}
-					label="Title"
-					variant="outlined"
-				/>
-				<FormControl fullWidth>
-					<InputLabel id="platform-label">Platform</InputLabel>
-
-					<Select
-						labelId="platform-label"
-						id="platform-select"
-						value={platform}
-						label="Platform"
-						onChange={handleChange}
-					>
-						{currentPlatformList.sort().map(val => (
-							<MenuItem key={val} value={val}>
-								{val}
-							</MenuItem>
-						))}
-						<ListSubheader>Other Platforms</ListSubheader>
-						{oldPlatformList.sort().map(val => (
-							<MenuItem key={val} value={val}>
-								{val}
-							</MenuItem>
-						))}
-					</Select>
-				</FormControl>
-				<Button onClick={() => handleSubmit()} variant="contained">
-					Submit
-				</Button>
+				{searchResults < 1 ? (
+					<>
+						<TextField
+							onChange={({ target }) => setQuery(target.value)}
+							label="Title"
+							variant="outlined"
+						/>
+						<Button onClick={() => handleSearch()} variant="contained">
+							Search
+						</Button>
+					</>
+				) : (
+					<>
+						<Select
+							labelId="game-select-label"
+							id="game-select"
+							value={selectedGame}
+							label="Game"
+							onChange={({ target }) => setSelectedGame(target.value)}
+						>
+							{searchResults.map(game => (
+								<MenuItem key={game.id} value={game.id}>
+									{game.name}
+								</MenuItem>
+							))}
+						</Select>
+						<Select
+							labelId="platform-select-label"
+							id="platform-select"
+							value={selectedPlatform ? selectedPlatform : ''}
+							label="Platform"
+							onChange={({ target }) => setSelectedPlatform(target.value)}
+						>
+							{gamePlatforms?.map(({ platform }) => (
+								<MenuItem key={platform.id} value={platform.name}>
+									{platform.name}
+								</MenuItem>
+							))}
+						</Select>
+						<Button onClick={handleClearState} variant="contained">
+							Search Again
+						</Button>
+						<Button onClick={handleSubmit} variant="contained">
+							Add Game
+						</Button>
+					</>
+				)}
+			</Box>
+			<Box sx={{ paddingY: 1, textAlign: 'center' }}>
+				<Typography>
+					Results provided by{' '}
+					<Link href="https://rawg.io/apidocs" target="blank">
+						RAWG API
+					</Link>
+				</Typography>
 			</Box>
 		</Dialog>
 	);
